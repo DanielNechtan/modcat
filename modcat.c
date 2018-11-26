ee74354ffb1508ff67cad2d43538ca371f50655c
int16h <int16h@openbsd.space>
2018-11-26 00:14:30 +0000

    sample names and lengths... not sure about malloc mess lol

--- modcat.c
+++ modcat.c
@@ -26,10 +26,10 @@
 
 static int verbose;
 
-struct
+typedef struct
 {
-	unsigned char name;	/* 22 bytes */
-	int length;		/* 2 bytes */
+	char *name;		/* 22 bytes */
+	char length;		/* 2 bytes */
 	int truelength;		/* truelength (in bytes) = length*2 */
 	char finetune;		/* 1 byte - lower 4 bits = 0x00-0x0F / upper 4 bits: 0 */
 	char volume;		/* 1 byte: 0x00-0x40 / 0-64 */
@@ -54,10 +54,38 @@ typedef struct
 } modcat_mod;
 
 int
-init_mod(modcat_mod *mod, char *md, int mdsz)
+load_samples(modcat_sample *sample, char *md)
+{
+	int i, n_samples;
+	n_samples = 0;
+	char *bp = md+20;
+        for(i = 1; i < 32; i++) {
+		sample = malloc(sizeof(*sample));
+
+		sample[i].name = malloc(23);
+		strlcpy(sample[i].name, bp, 22);
+
+		sample[i].length = bp[22];
+
+		if(sample[i].length > 0) {
+			printf("Sample %d: %s | length:%d\n",
+					i,
+					sample[i].name,
+					sample[i].length);
+			n_samples++;
+	         }
+	         bp += 30;
+        }
+/*        mod->n_samples = n_samples; */
+/*	printf("Samples: %d", mod->n_samples); */
+	return n_samples;
+}
+
+int
+init_mod(modcat_mod *mod, modcat_sample *smp, char *md, int mdsz)
 {
 	int i;
-	
+
 	mod->title = malloc(21);
 	strlcpy(mod->title, md, 20);
  	if(verbose)
@@ -78,12 +106,7 @@ init_mod(modcat_mod *mod, char *md, int mdsz)
 		printf("Format: %s\n", mod->format);
 	mod->patterns = &md[1084];
 
-	mod->n_samples = 0;
-	char *bp = md+20;
-	for(i = 0; i < 31; i++){
-		bp += 30;
-	}
-	mod->n_samples = i;
+	mod->n_samples = load_samples(smp, md);
 	if(verbose)
 		printf("Samples: %d\n", mod->n_samples);
 	return 1;
@@ -94,17 +117,19 @@ main(int argc, char *argv [])
 {
 	modcat_mod *module;
 	module = malloc(sizeof(*module));
+	modcat_sample *sample[31];
+/*	sample[31] = malloc(sizeof(*sample[31])); */
 	FILE *modfile;
 	char *moddata;
 	int datasz;
 	struct sio_hdl *hdl;
 	struct sio_par par;
-	
-	verbose = 0;	
 
-	if (argc < 2 || argc > 3) 
+	verbose = 0;
+
+	if (argc < 2 || argc > 3)
 		errx(1, "Usage: %s <file.mod>", argv[0]);
-	if ((modfile = fopen(argv[1], "rb")) == NULL) 
+	if ((modfile = fopen(argv[1], "rb")) == NULL)
 		err(1, "Could not open file");
 	if (argc == 3)
 		if(!strncmp(argv[2], "-v", 3))
@@ -114,7 +139,7 @@ main(int argc, char *argv [])
 	rewind(modfile);
 	if(verbose)
 		printf("Module is %d bytes\n", datasz);
-	if((moddata = malloc(datasz)) == NULL) 
+	if((moddata = malloc(datasz)) == NULL)
 		err(1, "malloc error");
 	if(!fread(moddata, datasz, 1, modfile)) {
 		free(moddata);
@@ -125,12 +150,19 @@ main(int argc, char *argv [])
 
 	/* parse MOD data */
 /*	memset(&module, '\0', sizeof(modcat_mod));*/
-	if(!init_mod(module, moddata, datasz))
+	if(!init_mod(module, sample, moddata, datasz))
 		err(1, "init_mod failed!");
- 	if(verbose) {
-
-	}	
+/*
+	if(!load_samples(module, sample, moddata, datasz))
+		err(1, "load_samples failed!");
+*/
+	if(verbose) {
+//		printf("samples: %d", module->n_samples);
+//		printf("Sample 0: %s", sample[1]->name);
+	}
 	/* tidy up for a clean exit() */
 	free(moddata);
+	free(module->title);
+	free(module->format);
 	return(1);
 }
